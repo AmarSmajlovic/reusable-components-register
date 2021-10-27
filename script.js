3e8e06375a913da8ac42683625050185fae25c54
AmarSmajlovic <amarcsgo@gmail.com>
2021-10-27 02:03:42 +0200

    added second form buttons radio and other elements

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 class RegisterFormFirst extends HTMLElement{
     connectedCallback(){
-        this.innerHTML = `
+      this.innerHTML = `
           <div class="form-container">
-           <h2>User register</h2>
-           <div class="form">
+          <div class="form">
+          <h2>User register</h2>
            <div class="input-form default">
            <input type="text" id="name">
            <label  for="name">Your Name <span class="important">*</span></label>
            <div class="alert-message">This is the description area</div>
-           <div class="description">This is the description area</div>
+           
            </div>
            <div class="input-form default">
            <input type="text" class="input" id="surname">
            <label  for="surname">Your Surname <span class="important">*</span></label>
            <div class="alert-message">This is an ERROR messagge</div>
-           <div class="description">This is the description area</div>
+ 
            </div>
            <div class="input-form default">
            <input type="email" class="input" id="email">
            <label for="email">email<span class="important">*</span></label>
            <div class="alert-message">This is a SUCCESS messagge</div>
-           <div class="description">This is the description area</div>
+           
            </div>
            <div class="input-form default">
            <input  type="password" class="input" name="password" id="password">
@@ -33,38 +33,109 @@ class RegisterFormFirst extends HTMLElement{
            </div>
            <div class="checkbox-form">
            <label class="switch" for="checkbox-terms">
-            <input  type="checkbox" id="checkbox-terms" />
+           <input  type="checkbox" id="checkbox-terms" />
            <div class="slider round"></div>
            </label>
            <p>I accept the general terms of use</p>
            </div>
            <div class="checkbox-form">
            <label class="switch" for="checkbox-privacy">
-            <input  type="checkbox" id="checkbox-privacy" />
+           <input  type="checkbox" id="checkbox-privacy" />
            <div class="slider round"></div>
            </label>
            <p>Privacy</p>
            </div>
-           <div class="container-radio">
-  <div class="radio">
-    <input  id="radio-1" name="radio" type="radio" checked>
-    <label for="radio-1" class="radio-label">Checked</label>
-  </div>
-
-  <div class="radio">
-    <input id="radio-2" name="radio" type="radio">
-    <label  for="radio-2" class="radio-label">Unchecked</label>
-  </div>
-
-  <div class="radio">
-    <input id="radio-3" name="radio" type="radio" disabled>
-    <label for="radio-3" class="radio-label">Disabled</label>
-  </div>
-</div>
+           <div class="buttons">
+           <button class="btn-outline large">CANCEL</button>
+           <button class="btn large">NEXT STEP</button>
            </div>
-          </div>
-        `
-    }
+  `
+}
 }
 
 customElements.define('register-form-first',RegisterFormFirst);
+
+class RegisterFormSecond extends HTMLElement{
+  connectedCallback(){
+    this.innerHTML = `
+    <div class="form-container">
+        <div class="form">
+           <div class="input-form default">
+           <select name="country" id="country">
+           <option>Select Country</option>
+           <option>United States</option>
+           </select>
+           <label  for="country">Country</label>
+           </div>
+           <div class="input-form default">
+           <select name="city" id="city">
+           <option>Select City</option>
+           <option>New York</option>
+           </select>
+           <label  for="city">City</label>
+           </div>
+           <div class="input-form default">
+           <input type="text" class="input" id="zip-code">
+           <label  for="zip-code">Zip Code</label>
+           </div>
+           <div class="adress-container">
+           <div class="input-form default">
+           <input type="text" class="input" id="adress">
+           <label  for="adress">Adress</label>
+           </div>
+           <div class="input-form default">
+           <input type="text" class="input" id="number">
+           </select>
+           <label  for="number">Number</label>
+           </div>
+           </div>
+           <div class="input-form default">
+           <input type="text" class="input" id="id-card">
+           </select>
+           <label  for="id-card">Personal ID Card</label>
+           </div>
+        </div>
+        <div class="form">
+        <div class="input-form default">
+        <input type="text" class="input" id="id-card">
+        </select>
+        <label  for="id-card">Date of birth</label>
+        </div>
+        <div class="container-radio">
+        <div class="radio">
+          <input  id="male-radio" name="male-radio" type="radio">
+          <label for="male-radio" class="radio-label">Male</label>
+        </div>
+        <div class="radio">
+          <input id="female-radio" name="female-radio" type="radio">
+          <label  for="female-radio" class="radio-label">Female</label>
+        </div>
+        <div class="radio">
+          <input id="other-radio" name="other-radio" type="radio">
+          <label for="other-radio" class="radio-label">Other</label>
+        </div>
+      </div>
+      <div class="input-form default">
+      <input type="text" class="input" id="phone-number">
+      </select>
+      <label  for="phone-numer">Phone Number</label>
+      </div>
+      <div class="input-form default">
+      <input type="text" class="input" id="account-number">
+      </select>
+      <label  for="account-numer">Account Number</label>
+      </div>
+      <div class="input-form default">
+      <input type="text" class="input" id="account-number">
+      </select>
+      <label  for="account-numer">Account Number</label>
+      </div>
+      <button class="btn medium" id="register-btn">REGISTER</button>
+      </div>
+        </div>
+    </div>
+    `
+  }
+}
+
+customElements.define('register-form-second',RegisterFormSecond);
